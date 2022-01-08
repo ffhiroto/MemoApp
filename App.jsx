5193d6386f3bde021c41ac40ce3d7873faed41fe
ffhiroto <94722249+ffhiroto@users.noreply.github.com>
2022-01-08 15:45:20 -0600

    extract components

--- App.jsx
+++ App.jsx
@@ -1,52 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+import AppBar from "./src/components/AppBar";
+import MemoList from "./src/components/MemoList";
+import CircleButton from "./src/components/CircleButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>Memo App</Text>
-          <Text style={styles.appbarRight}>Logout</Text>
-        </View>
-      </View>
-
-      <View>
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>Purchase list</Text>
-            <Text style={styles.memoListItemDate}>2021/10/2 12:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>Purchase list</Text>
-            <Text style={styles.memoListItemDate}>2021/10/2 12:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>Purchase list</Text>
-            <Text style={styles.memoListItemDate}>2021/10/2 12:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-      </View>
-
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabbel}>+</Text>
-      </View>
+      <AppBar />
+      <MemoList />
+      <CircleButton>+</CircleButton>
     </View>
   );
 }
@@ -55,66 +20,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F0F4F8",
-  },
-  appbar: {
-    width: "100%",
-    height: 104,
-    backgroundColor: "#467FD3",
-    justifyContent: "flex-end",
-  },
-  appbarInner: {
-    alignItems: "center",
-  },
-  appbarTitle: {
-    fontSize: 22,
-    lineHeight: 32,
-    color: "#ffffff",
-    fontWeight: "bold",
-    marginBottom: 8,
-  },
-  appbarRight: {
-    position: "absolute",
-    color: "rgba(255, 255, 255, 0.8)",
-    right: 19,
-    bottom: 12,
-  },
-  memoListItem: {
-    backgroundColor: "#ffffff",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: "center",
-    borderBottomWidth: 1,
-    borderColor: "rgba(0, 0, 0, 0.15)",
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: "#848484",
-  },
-  circleButton: {
-    backgroundColor: "#467FD3",
-    width: 64,
-    height: 64,
-    borderRadius: 34,
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    right: 40,
-    bottom: 40,
-    shadowColor: "#000000",
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.3,
-    shadowRadius: 8,
-  },
-  circleButtonLabbel: {
-    color: "#ffffff",
-    fontSize: 40,
-    lineHeight: 40,
   },
 });
